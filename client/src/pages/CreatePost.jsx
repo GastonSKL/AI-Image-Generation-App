25cce66e7e9116eccf3fbfeec5fd43972cf959c2
GastonSKL <gastonlskamlec@gmail.com>
2024-09-25 18:08:26 -0300

    Small corrections

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -81,7 +81,7 @@ function CreatePost() {
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
         <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
-          Create imaginative and visually stunning throughStable Diffusion and share
+          Create imaginative and visually stunning through Stable Diffusion and share
           it with the comunity.
         </p>
       </div>
