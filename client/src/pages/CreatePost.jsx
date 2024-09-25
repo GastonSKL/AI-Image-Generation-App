404aeff6f00eccabb3c7d9404c5d0f2912120d38
GastonSKL <gastonlskamlec@gmail.com>
2024-09-25 12:06:21 -0300

    [add][25/09/2024][GenerativeApp] Adding structure to the create post page

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { preview } from "../assets";
+import { getRandomPrompt } from "../utils";
+import { FormField, Loader } from "../components";
 
 function CreatePost() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    prompt: "",
+    photo: "",
+  });
+
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) =>{}
+  const handleChange = (e) =>{}
+  const handleSurpriseMe = () =>{}
+
   return (
-    <div>CreatePost</div>
-  )
+    <section className="max-w-7xl mx-auto">
+      <div>
+        <h1 className="font-extrabold text-[#222328] text-[32px]">
+          Create
+        </h1>
+        <p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
+          Create imaginative and visually stunning
+          through DALEE-e AI and share it with the comunity.
+        </p>
+      </div>
+      <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-5">
+          <FormField 
+            labelName="Your name"
+            type="text"
+            name="name"
+            placeholder="John Dor"
+            value={form.name}
+            handleChange={handleChange}
+          />
+          <FormField 
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A pencil and watercolor drawing of a bright city in the future with flying cars"
+            value={form.prompt}
+            handleChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
+          />
+        </div>
+      </form>
+    </section>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
