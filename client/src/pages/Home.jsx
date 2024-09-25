e3286498c6b99c2ae51650c5f083a03659cb1662
GastonSKL <gastonlskamlec@gmail.com>
2024-09-25 17:35:05 -0300

    changed url to match deployed backend

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
     const fetchPost = async () => {
       setLoading(true);
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
+        const response = await fetch("https://ai-image-generation-app-do6m.onrender.com/api/v1/post", {
           method: "GET",
           headers: {
             "Content-type": "application/json",
